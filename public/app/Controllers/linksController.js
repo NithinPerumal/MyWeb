c3c9403b8ef744972cd76f671fed908cf10bfc8a
Nithin Perumal <Nithin_Perumal@intuit.com>
2016-08-23 16:02:47 -0700

    Final Site

--- public/app/Controllers/linksController.js
+++ public/app/Controllers/linksController.js
@@ -1,8 +1,18 @@
 /**
  * Created by nperumal on 8/4/16.
  */
-app.controller("linksCtrl", ['$scope', '$location', '$http', '$q', function($scope, $location, $http, $q) {
+app.controller("linksCtrl", ['$scope', '$location', '$http', '$q', '$window', function($scope, $location, $http, $q, $window) {
 
     console.log("linksCtrl");
+
+    $scope.redirectToGithub = function() {
+        console.log("Redirecting to GitHub");
+        $window.open("https://github.com/NithinPerumal/");
+    };
+
+    $scope.redirectToLinkedIn = function() {
+        console.log("Redirecting to LinkedIn");
+        $window.open("https://www.linkedin.com/in/nithin-perumal-1268b8a9");
+    };
 
 }]);
